05922b7afc0382d062581c578d9d5e28960e17d1
Artem Lyu <alyu3@wisc.edu>
2024-08-07 21:22:23 -0500

    in process of making sign-in

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client"
 import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 import bcrypt from 'bcrypt';
+import { signInSchema } from "./lib/zod"
 
 const prisma = new PrismaClient()
 
@@ -14,6 +15,35 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       Google({
         clientId: process.env.AUTH_GOOGLE_ID ?? "",
         clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+      }),
+      Credentials({
+        credentials: {
+          email: {label : "Email", type: "email", placeholder: "email@example.com"},
+          password: {label: "Password", type: "password"},
+        },
+        authorize: async(credentials) => {
+          if (!credentials?.email || !credentials.password) {
+            return null;
+          }
+
+          const { email, password } = await signInSchema.parseAsync(credentials)
+          
+          const user = await prisma.user.findUnique({
+            where: {email : email as string}
+          })
+
+          if (!user) {
+            throw new Error('User not found')
+          }
+
+          const pwHashed = await bcrypt.hash(password as string, 10)
+
+
+          return user
+        }
       })
     ],
 })
+
+
+
