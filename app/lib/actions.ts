84b586031d4a8e82bfb4014ec9a10a54488d8e8b
Artem Lyu <alyu3@wisc.edu>
2024-08-10 14:48:52 -0500

    cleaned up error returning in actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,30 +1,20 @@
 'use server';
- 
-import { signIn } from '@/auth';
-import { redirect } from 'next/navigation';
-import { AuthError } from 'next-auth';
- 
-export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData,
-  ) {
-    try {
-      await signIn('credentials', formData);
 
-      redirect("/landing")
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.message) {
-          case 'Missing credentials. Read more at https://errors.authjs.dev#autherror':
-            return 'Please enter your credentials';
-          case 'User not found. Read more at https://errors.authjs.dev#autherror':
-            return 'User not found.';
-          case 'Invalid user or password is incorrect. Read more at https://errors.authjs.dev#autherror':
-            return 'Invalid user or password is incorrect';
-          default:
-            return 'Something went wrong.';
-        }
-      }
-      throw error;
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      const cleanedMessage = error.message.replace(/\. Read more at .+$/, '');
+      return cleanedMessage
     }
+    console.error(error);
+    return 'An unexpected error occurred';
   }
+}
--- auth.ts
+++ auth.ts
@@ -5,14 +5,12 @@ import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 import bcrypt from 'bcrypt';
 import { signInSchema } from "./lib/zod"
+import { ZodError } from "zod"
 import { redirect } from "next/navigation"
 
 const prisma = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  pages: {
-    signIn: '/login'
-  },
   adapter: PrismaAdapter(prisma), // defaults session strategy to database
   providers:
     [
@@ -26,42 +24,50 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           password: { label: "Password", type: "password" },
         },
         authorize: async (credentials) => {
-            if (!credentials.email || !credentials.password) {
+          try {
+
+            if (!credentials?.email || !credentials?.password) {
               throw new AuthError("Missing credentials");
             }
-        
-            const { email, password } = await signInSchema.parseAsync(credentials);
-        
+
+            const { email, password } = await signInSchema.parse(credentials);
+
             const user = await prisma.user.findUnique({
               where: { email: email as string },
             });
-        
+
             if (!user) {
               throw new AuthError("User not found");
             }
-        
+
             const userId = await prisma.account.findUnique({
               where: {
                 provider_providerAccountId: {
                   provider: 'credentials',
-                  providerAccountId: email,
+                  providerAccountId: email as string,
                 },
               },
             });
-        
+
             if (!userId) {
               throw new AuthError("Invalid user or password is incorrect");
             }
-        
+
             const compare = await bcrypt.compare(password as string, userId?.password as string);
-        
+
             if (!compare) {
               throw new AuthError("Invalid user or password is incorrect");
             }
-        
             return user;
-      
-          
+          } catch (error) {
+            if (error instanceof ZodError) {
+              throw new AuthError(`${error.errors.map(e => e.message).join(", ")}`);
+            }
+            if (error instanceof AuthError) {
+              throw new AuthError(error.message);
+            }
+            throw error
+          }
         }
       })
     ],
