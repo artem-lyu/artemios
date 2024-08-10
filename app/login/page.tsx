ac5546c9aaca81f8cb76e6a50ade17de6272d999
Artem Lyu <alyu3@wisc.edu>
2024-08-10 12:47:03 -0500

    added error handling client side to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,41 @@
-import { SignIn } from "@/components/sign-in"
+"use client"
+
 import { SignInGoogle } from "@/components/ui/sign-in-button-google"
+import { authenticate } from "../lib/actions"
+import { useFormState } from "react-dom";
+import { TriangleAlert } from "lucide-react";
+
 export default function LoginPage() {
+
+  const [errorMessage, formAction, isPending] = useFormState(
+    authenticate,
+    undefined,
+  );
+
   return (
     <>
-      <SignIn />
-      <div className="width">
+      <form
+        action={formAction}
+      >
+        <label>
+          Email
+          <input name="email" type="email" />
+        </label>
+        <label>
+          Password
+          <input name="password" type="password" />
+        </label>
+        <button>Sign In</button>
+        {errorMessage && (
+          <>
+            <TriangleAlert className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          </>
+        )}
+      </form>
+      {/* <div className="width">
         <SignInGoogle />
-      </div>
+      </div> */}
     </>
   )
 }
