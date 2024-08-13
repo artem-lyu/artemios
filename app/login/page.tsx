1a4a2ecae0f4b09e16500348737727d0389e5359
Artem Lyu <alyu3@wisc.edu>
2024-08-13 15:20:36 -0500

    improved login page front end and changed home page svg

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { authenticate} from "../lib/actions"
+import { authenticate } from "../lib/actions"
 import { useFormState } from "react-dom";
 import { TriangleAlert } from "lucide-react";
 import { useSearchParams } from "next/navigation";
+import { motion } from "framer-motion";
 
 export default function LoginPage() {
 
@@ -20,26 +21,36 @@ export default function LoginPage() {
   }
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-500">
+      <div className="relative w-full flex justify-center align-top">
+        <h1 className="font-absans text-9xl">ARTEMIOS</h1>
+      </div>
       <form
         action={formAction}
       >
-        
-        <input type="hidden" name="authType" value="credentials"/>
-        <div>
-        <label>
-          Email
-          <input name="email" type="email" className="border-2 block" />
-        </label>
+
+        <input type="hidden" name="authType" value="credentials" />
+        <div className="py-4">
+          <label className="font-absans text-2xl ">
+            Email
+            <input name="email" type="email" className="border-black block bg-slate-400" />
+          </label>
         </div>
-        <div>
-        <label>
-          Password
-          <input name="password" type="password" className="border-2 block"/>
-        </label>
+        <div className="py-4">
+          <label className="font-absans text-2xl">
+            Password
+            <input name="password" type="password" className="border-black block bg-slate-400" />
+          </label>
         </div>
-        <div className="py-2">
-        <button className="text-white w-30  bg-[#4285F4] hover:bg-[#4285F4]/90 hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded">Sign In</button>
+        <div className="flex flex-col py-4 items-center justify-center font-absans">
+          <motion.button
+            className="bg-transparent  text-black py-2 px-4 border-2 border-black rounded-full"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.2 }} // This transition only applies to the hover effect
+          >
+            Sign in
+          </motion.button>
         </div>
         {errorMessage && (
           <>
@@ -49,12 +60,12 @@ export default function LoginPage() {
         )}
       </form>
       <div className="width">
-    <form action={formAction}>
-      <input type="hidden" name="authType" value="google"/>
-    <button type="submit" className="text-white w-30  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2"><svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>Sign in with Google<div></div></button>
-    </form>
+        <form action={formAction}>
+          <input type="hidden" name="authType" value="google" />
+          <button type="submit" className="text-white w-30  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2"><svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>Sign in with Google<div></div></button>
+        </form>
       </div>
       <p>Don't have an account? <a href="/register" className="text-blue-600 dark:text-blue-500 hover:underline">Register</a></p>
-    </>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,45 @@ import { TypeAnimation } from 'react-type-animation';
 import { LogInIcon, TentTree } from 'lucide-react';
 import Image from 'next/image';
 import undraw_runner2 from '@/public/images/undraw_runner_start_x-0-uu.svg';
+import undraw_runner from '@/public/images/undraw_different_love_a-3-rg.svg';
 import undraw_hiking from '@/public/images/undraw_hiking_re_k0bc.svg';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 
 export default async function Page() {
-
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
+    <>
     <div className="w-screen h-screen bg-slate-500">
-      <a href="/login">
-        <LogInIcon className='absolute top-0 right-0 h-24 w-24 px-4 py-4 mt-24 mr-24' />
-      </a>
+      <div>
+        <a href="/login">
+          <motion.div
+            whileHover={{
+              opacity: 0.6,
+            }}
+          >
+            <LogInIcon className='absolute top-0 right-0 h-24 w-24 px-4 py-4 mt-24 mr-24' />
+          </motion.div>
+        </a>
+      </div>
       <div className='mt-24 ml-24 mr-24 font-absans pr-[calc(50%-200px)]'>
-        <h1 className=" text-9xl">WELCOME</h1>
-
+        <h1 className=" text-9xl">WELCOME TO ARTEMIOS</h1>
         <TypeAnimation
           speed={55}
           sequence={[
             "We're so glad you could make it!",
             1000,
             "We're so glad you could make it!\nWe're here to help you grow! ",
-            () => {
-              console.log('Sequence completed');
-            },
           ]}
-          wrapper="span" cursor={true} repeat={1} style={{ fontSize: '2em', display: 'inline-block', fontFamily: 'absans-regular', whiteSpace: 'pre-line', height: '195px' }} />
+          wrapper="span" cursor={false} repeat={0} style={{ fontSize: '2em', display: 'inline-block', fontFamily: 'absans-regular', whiteSpace: 'pre-line', height: '195px' }} />
 
-        <div className='mt-8 mr-96 flex justify-center font-absans'>
+        <div className='mt-4 mr-96 flex justify-center font-absans'>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ delay: 4, duration: 1 }}
+            transition={{ delay: 5, duration: 2 }}
           >
             <motion.button
               className="bg-transparent  text-black py-2 px-4 border-2 border-black rounded-full"
@@ -48,11 +55,12 @@ export default async function Page() {
         </div>
 
         <div className=" absolute px-32 right-0 top-1/2 transform -translate-y-1/2 mr-24">
-          <Image src={undraw_hiking} alt="Runner" className='opacity-75' />
+          <Image src={undraw_runner} alt="Runner" className='opacity-75' />
         </div>
 
       </div>
     </div >
+    </>
   );
 }
 
