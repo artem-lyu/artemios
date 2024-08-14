9d0f7a8cb9ac442141380e5f042d1ad34e06ba2c
Artem Lyu <alyu3@wisc.edu>
2024-08-14 15:27:48 -0500

    attempted to make everything dynamically render according to screen size

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,64 @@
-"use client";
+"use client"
 import { TypeAnimation } from 'react-type-animation';
-import { LogInIcon } from 'lucide-react';
+import { LogInIcon, TentTree } from 'lucide-react';
 import Image from 'next/image';
+import undraw_runner2 from '@/public/images/undraw_runner_start_x-0-uu.svg';
 import undraw_runner from '@/public/images/undraw_different_love_a-3-rg.svg';
+import undraw_hiking from '@/public/images/undraw_hiking_re_k0bc.svg';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
+
 
 export default async function Page() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <>
-      <div className="w-screen h-screen bg-slate-500 flex flex-col items-center justify-center">
-        <div className="absolute top-0 right-0 p-6">
-          <a href="/login">
-            <motion.div whileHover={{ opacity: 0.6 }}>
-              <LogInIcon className="h-16 w-16" />
-            </motion.div>
-          </a>
-        </div>
-
-        <div className="right-0 top-1/2">
-          <Image
-            src={undraw_runner}
-            alt="Runner"
-            className="opacity-75 w-48 h-auto sm:w-64 md:w-80 lg:w-96"
-          />
-        </div>
-
-        <div className="text-center p-8 max-w-screen-lg mx-auto">
-          <h1 className="text-5xl sm:text-7xl md:text-9xl">WELCOME TO ARTEMIOS</h1>
-          <TypeAnimation
-            speed={55}
-            sequence={[
-              "We're so glad you could make it!",
-              1000,
-              "We're so glad you could make it!\nWe're here to help you grow!",
-            ]}
-            wrapper="span"
-            cursor={false}
-            repeat={0}
-            className="block mt-4 text-xl sm:text-2xl md:text-3xl"
-            style={{ whiteSpace: 'pre-line', height: 'auto' }}
-          />
-
-          <div className="mt-8">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 5, duration: 2 }}
-            >
-              <motion.button
-                className="bg-transparent text-black py-2 px-4 border-2 border-black rounded-full"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-                transition={{ duration: 0.2 }}
-              >
-                Get started
-              </motion.button>
-            </motion.div>
-          </div>
-        </div>
-
-
+    <div className="w-screen h-screen bg-slate-500 flex flex-col relative">
+      <div className='relative'>
+        <a href="/login">
+          <motion.div
+            whileHover={{
+              opacity: 0.6,
+            }}
+          >
+            <LogInIcon className='absolute top-0 right-0 h-12 w-12 mt-[2vh] mr-[2vw]' />
+          </motion.div>
+        </a>
       </div>
+      <div className='relative flex flex-col mt-auto ml-auto mr-auto font-absans'>
+        <h1 className=" text-4xl md:text-6xl lg:text-9xl">WELCOME TO ARTEMIOS</h1>
+        <TypeAnimation
+          speed={55}
+          sequence={[
+            "We're so glad you could make it!",
+            1000,
+            "We're so glad you could make it!\nWe're here to help you grow! ",
+          ]}
+          wrapper="span" cursor={false} repeat={0} style={{ fontSize: '2em', display: 'inline-block', fontFamily: 'absans-regular', whiteSpace: 'pre-line', height: '195px' }} />
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 5, duration: 2 }}
+          >
+            <motion.button
+              className="bg-transparent  text-black py-2 px-4 border-2 border-black rounded-full"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ duration: 0.2 }} // This transition only applies to the hover effect
+            >
+              Get started
+            </motion.button>
+          </motion.div>
+      </div>
+
+      <div className="relative flex flex-col w-auto justify-center items-center h-auto mb-[2vh]">
+          <Image src={undraw_runner} alt="Runner" className='responsive-image mx-auto opacity-75' />
+        </div>
+
+    </div >
     </>
   );
 }
+
