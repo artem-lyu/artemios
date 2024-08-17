496979fbfccee209f6ef80b6952360c55551ab58
Artem Lyu <alyu3@wisc.edu>
2024-08-17 00:05:16 -0500

    started working on a grid layout in the landing page (tbd dashboard)

--- app/dashboard/landing/page.tsx
+++ app/dashboard/landing/page.tsx
@@ -36,23 +36,24 @@ export default async function HomePage() {
 
     return (
         <div className="flex w-full h-full bg-slate-500">
-            <div className="flex-1">
-                <h1 className="font-kaisei text-5xl text-center">Hi {session?.user?.name}, how are you today?</h1>
-                <Chat accessToken={accessToken} />
-                <div>
-                    <h1 className="text-5xl py-4 pt-6 text-center">Latest Sessions</h1>
+            <div className="flex flex-row mx-5 my-5 border flex-1">
+                <div className="flex-col mx-5">
+                    <h1 className="font-kaisei text-5xl text-center">Hi {session?.user?.name}, how are you today?</h1>
+                    <Chat accessToken={accessToken} />
                 </div>
-                <div className="flex-auto flex-col text-center justify-center">
-
-                    <ul className="">
-                        {latestSessions.map((session: any) => (
-                            <li key={session.id} className="p-2 my-2">
-                                <Link href={`/dashboard/sessions/transcript/${session.id}`} className="inline-block">
-                                    <p className="hover:underline">{new Date(session.date).toLocaleString()}</p>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
+                <div className="flex-col mx-5">
+                    <h1 className="text-5xl text-center">Latest Sessions</h1>
+                    <div className="flex-auto flex-col text-center justify-center">
+                        <ul className="">
+                            {latestSessions.map((session: any) => (
+                                <li key={session.id} className="p-2 my-2">
+                                    <Link href={`/dashboard/sessions/transcript/${session.id}`} className="inline-block">
+                                        <p className="hover:underline">{new Date(session.date).toLocaleString()}</p>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
