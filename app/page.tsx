607ff22dccf38c3943756b058c20cdc1ba6de280
Artem Lyu <alyu3@wisc.edu>
2024-08-18 23:05:06 -0500

    added description on home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,50 +13,50 @@ import { useState } from 'react';
 export default async function Page() {
 
   return (
-      <div className="h-screen bg-slate-500 flex flex-col relative ">
-        <div className='relative flex flex-col mx-[6vw] mt-[6vh] font-absans'>
+    <div className=" bg-slate-500 flex h-screen flex-1 relative ">
+      <div className='relative flex flex-1 flex-col mx-[6vw] my-[6vh] font-absans'>
 
         <div className="flex justify-between items-start ">
-            <h1 className="text-4xl md:text-6xl lg:text-9xl">WELCOME TO ARTEMIOS</h1>
-            <a href="/dashboard/landing">
-              <motion.div
-                whileHover={{
-                  opacity: 0.6,
-                }}
-              >
-                <LogInIcon className='h-12 w-12' />
-              </motion.div>
-            </a>
-          </div>
-          <TypeAnimation
-            speed={55}
-            className="text-xl md:text-2xl lg:text-3xl"
-            sequence={[
-              "We're so glad you could make it!",
-              1000,
-              "We're so glad you could make it!\nWe're here to help you grow. ",
-            ]}
-            wrapper="span" cursor={false} repeat={0} style={{display: 'inline-block', fontFamily: 'absans-regular', whiteSpace: 'pre-line', height: '195px' }} />
-
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 5, duration: 2 }}
-          >
-            <motion.button
-              className="bg-transparent  text-black py-2 px-4 border-2 border-black rounded-full"
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              transition={{ duration: 0.2 }} // This transition only applies to the hover effect
+          <h1 className="text-4xl md:text-6xl lg:text-9xl">WELCOME TO ARTEMIOS</h1>
+          <a href="/dashboard/landing">
+            <motion.div
+              whileHover={{
+                opacity: 0.6,
+              }}
             >
-              Get started
-            </motion.button>
-          </motion.div>
-          {/* <div>
-            <Image src={undraw_runner} alt="Runner" className='mr-0 mb-0 opacity-75 object-contain' />
-          </div> */}
+              <LogInIcon className='h-12 w-12' />
+            </motion.div>
+          </a>
         </div>
-      </div >
+        <TypeAnimation
+          speed={55}
+          className="text-xl md:text-2xl lg:text-3xl"
+          sequence={[
+            "We're so glad you could make it!",
+            1000,
+            "We're so glad you could make it!\nWe're here to help you grow. ",
+          ]}
+          wrapper="span" cursor={false} repeat={0} style={{ display: 'inline-block', fontFamily: 'absans-regular', whiteSpace: 'pre-line', height: '195px' }} />
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 5, duration: 2 }}
+        >
+          <motion.button
+            className="bg-transparent  text-black py-2 px-4 border-2 border-black rounded-full"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ duration: 0.2 }} // This transition only applies to the hover effect
+          >
+            Get started
+          </motion.button>
+        </motion.div>
+        <div className='flex flex-1 items-end justify-end'>
+          <p className='max-w-xl text-left mx-4 my-4 text-2xl rounded-lg p-5'>ARTEMIOS is a powerful tool for anyone looking to gain a deeper understanding of their emotional health. By providing personalized insights and an engaging user experience, we help users take proactive steps towards improving their emotional well-being.</p>
+        </div>
+      </div>
+    </div >
   );
 }
 
